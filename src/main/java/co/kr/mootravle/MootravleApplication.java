605343b0ef41fef7c142a1580d42020d04dba9c3
moosoony <moogyong@naver.com>
2023-02-17 14:21:59 +0900

    Initial commit

---
+++ src/main/java/co/kr/mootravle/MootravleApplication.java
@@ -0,0 +1,13 @@
+package co.kr.mootravle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MootravleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MootravleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/mootravle/MootravleApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.mootravle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MootravleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
