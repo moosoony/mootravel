e815fe66cfa3e24b128d6c9b5d309a7d5f722519
moomoo <moosoon97@gmail.com>
2023-02-26 13:48:55 +0900

    - navigation 로그인한 사용자 이름 출력 완료

--- src/main/java/kr/co/mootravle/MainController.java
+++ src/main/java/kr/co/mootravle/MainController.java
@@ -1,7 +1,10 @@
 package kr.co.mootravle;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class MainController {
